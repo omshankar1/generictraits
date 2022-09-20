b1992a53f0aa437ea455a91b28bac7f788f66839
Shankar Nagarajan <shankar.nagarajan@firstwavecloud.com>
2022-09-21 03:50:32 +1000

    refactor

--- src/distance.rs
+++ src/distance.rs
@@ -1,5 +1,6 @@
 use std::ops::Add;
 
+// ****** Struct Meter ***************************
 #[derive(Debug, Clone, Copy)]
 pub struct Meter(pub f32);
 
@@ -8,24 +9,28 @@ impl Meter {
         Self(val)
     }
 }
+// Impl of Trait: Add - Millimeter + Millimeter
 impl Add<Self> for Millimeter {
     type Output = Self;
     fn add(self, other: Millimeter) -> Self {
         Self::new(self.0 + other.0)
     }
 }
+// Impl of Trait: Add - Millimeter + Meter
 impl Add<Meter> for Millimeter {
     type Output = Self;
     fn add(self, other: Meter) -> Self {
         Self::new(self.0 + (other.0 * 1000.0))
     }
 }
+// Impl of Trait: From
 impl From<Meter> for Millimeter {
     fn from(m: Meter) -> Self {
         Self::new(m.0 * 100.0)
     }
 }
 
+// ****** Struct Millimeter ***************************
 #[derive(Debug, Clone, Copy)]
 pub struct Millimeter(pub f32);
 impl Millimeter {
@@ -33,12 +38,14 @@ impl Millimeter {
         Self(val)
     }
 }
+// Impl of Trait: Add Meter + Meter
 impl Add<Self> for Meter {
     type Output = Self;
     fn add(self, other: Meter) -> Self {
         Self::new(self.0 + other.0)
     }
 }
+// Impl of Trait: Add Meter + Millimeter
 impl Add<Millimeter> for Meter {
     type Output = Self;
     fn add(self, other: Millimeter) -> Self {
@@ -46,6 +53,7 @@ impl Add<Millimeter> for Meter {
     }
 }
 
+// Impl of Trait: From
 impl From<Millimeter> for Meter {
     fn from(mm: Millimeter) -> Self {
         Self::new(mm.0 / 100.0)
